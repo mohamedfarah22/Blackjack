74106a62ce7ea0e68c950a1174e47861ea71253f
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2022-06-02 21:29:30 +1000

    added reverse linkedlist functionality

--- decklist.py
+++ decklist.py
@@ -8,7 +8,7 @@ class Deck:
     def shuffle_deck(self):
         return random.shuffle(self.deck)
 class Player:
-    def __init__(self,name,next_player):
+    def __init__(self,name,next_player=None):
         self.name=name
         self.next_player = next_player
         self.cards=[]
@@ -58,8 +58,22 @@ class LinkPlayers:
                                         current_player = None
                                 else:
                                         current_player = next_player
-
-
+        def reverse_list(self):
+                i = 0
+                current_player = self.head_player
+                player_list = []
+                while current_player:
+                        i+=1
+                        player_list.append(current_player.get_player())
+                        current_player = current_player.get_next_player()
+                print(player_list)
+                j = 0
+                players_link_list = LinkPlayers(player_list[j])
+                j+=1
+                for player in player_list:
+                        players_link_list.insert_player(player)
+                print(players_link_list.list_of_players())
+        
 
 
 
@@ -81,16 +95,16 @@ def deck(list):
 
          
 def get_num_players():
-        num_players=input("How many people will be playing this round? Please note there is a maximum of 3 people: ")
+        num_players=input("How many people will be playing this round? Please note there is a maximum of 6 people: ")
         try:
                 int(num_players)
         except ValueError:
                 print("Input is not a number! Try Again!")
                 get_num_players()
-        var = input("There will be " +  num_players +  " in the next blackjack game. Press c to confirm or t to try again: ")
+        var = input("There will be " +  num_players +  " in the next blackjack game. Press any button to confirm or t to try again: ")
         
 
-        if int(num_players)>7:
+        if int(num_players)>6:
                 print("Wrong input! Please try again!")
                 get_num_players()
         if var.lower()=="t":
@@ -111,6 +125,25 @@ def blackjack():
       print("Welcome to blackjack!")
       var = input("Please press any letter on your keyboard to continue: ")
       num_players=get_num_players()
+      player_names={}
+      player_num=1
+      while player_num<num_players+1:
+              player_names[player_num] = input("Please input player {} name: ".format(player_num))
+              player_num+=1
+      players=LinkPlayers("The Dealer")
+      for i in range(1,num_players+1):
+        players.insert_player(player_names[i])
+      print(players.list_of_players())
+      players.reverse_list()
+      # reverse the linked_list before starting next game
+
+
+
+
+
+
+
+              
       
 
 
